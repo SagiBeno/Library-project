16f58fd2e2cd011a81b8edb7641dc6b87eb8e788
SagiBeno <benosagi321@gmail.com>
2025-12-11 23:11:44 +0100

    Implement Spinner

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -6,6 +6,7 @@ import MyBooksPage from './Pages/MyBooksPage'
 import AdminPage from './Pages/AdminPage'
 import ProfilePage from './Pages/ProfilePage'
 import RegisterPage from './Pages/RegisterPage'
+import Spinner from './Components/Spinner'
 import supabase from "./supabase-test/supabase";
 import { Container } from "@mui/material"
 import './App.css'
@@ -16,6 +17,7 @@ export default function App(props) {
   const [loggedIn, setLoggedIn] = useState(true);
   const [isAdmin, setIsAdmin] = useState(true);
   const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -32,12 +34,15 @@ export default function App(props) {
       <Navbar isAdmin={isAdmin} setLoggedIn={setLoggedIn} loggedIn={loggedIn} />
 
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/my-books' element={<MyBooksPage />} />
-        <Route path='/admin' element={<AdminPage />} />
-        <Route path='/profile' element={<ProfilePage />} />
-        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/' element={<HomePage setIsLoading={setIsLoading} />} />
+        <Route path='/my-books' element={<MyBooksPage setIsLoading={setIsLoading} />} />
+        <Route path='/admin' element={<AdminPage setIsLoading={setIsLoading} />} />
+        <Route path='/profile' element={<ProfilePage setIsLoading={setIsLoading} />} />
+        <Route path='/register' element={<RegisterPage setIsLoading={setIsLoading} />} />
       </Routes>
+
+      {isLoading && <Spinner />}
+
 
       {JSON.stringify(users)}
     </>
---
+++ react-app/src/Components/Spinner.jsx
@@ -0,0 +1,21 @@
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+
+export default function Spinner(props) {
+  return (
+    <Box
+        sx={{ 
+            display: 'flex',
+            minHeight: '100%',
+            minWidth: '100%',
+            justifyContent: 'center',
+            alignItems: 'center',
+            position: 'absolute',
+            zIndex: '1',
+            top: 0
+        }}
+    >
+      <CircularProgress size="3rem" />
+    </Box>
+  );
+}
