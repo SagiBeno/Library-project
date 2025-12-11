e6e6aa7c3707a9b78a729e99f72f2a7deb038a6f
SagiBeno <benosagi321@gmail.com>
2025-12-11 11:33:30 +0100

    Preparing the navigation bar

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,25 +1,17 @@
 import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './Pages/HomePage'
+import Navbar from './Components/Navbar'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<HomePage />}/>
+      </Routes>
     </>
   )
 }
