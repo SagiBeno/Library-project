33668d6189c647e7a9459539997ee852208536c5
SagiBeno <benosagi321@gmail.com>
2025-12-13 12:25:55 +0100

    Create radio buttons components

--- react-app/src/Pages/AdminPage.jsx
+++ react-app/src/Pages/AdminPage.jsx
@@ -1,7 +1,26 @@
+import { Container } from "@mui/material"
+import { useState } from "react"
+import RadioButtons from "../Components/RadioButtons";
+import Table from "../Components/Table";
+
 export default function AdminPage(props) {
+    const [radioOptions, setRadioOptions] = useState({
+        worker: 'Worker',
+        member: 'Member',
+        admin: 'Admin'
+    });
+
+    const [selectedOption, setSelectedOption] = useState('');
+
+    const handleRadioButtons = (value) => {
+        console.log(value);
+        setSelectedOption(value);
+        // TODO - Querying relevant data
+    }
+
     return (
-        <>
-            <h1>AdminPage</h1>
-        </>
+        <Container className="container">
+            <RadioButtons radioOptions={radioOptions} handleRadioButtons={handleRadioButtons}/>
+        </Container>
     )
 }
