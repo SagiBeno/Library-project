e8917e55f3a28aef92cfd5802afff3c0673ba86d
SagiBeno <benosagi321@gmail.com>
2025-12-14 14:41:46 +0100

    Show data in AdminPage

---
+++ react-app/src/Components/MyTable.jsx
@@ -0,0 +1,43 @@
+import { Table, TableContainer, TableHead, TableBody, TableRow, TableCell, Paper, IconButton } from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+
+export default function MyTable( { data, handleEdit, handleDelete } ) {
+
+    return (
+        <TableContainer component={Paper}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Id</TableCell>
+                        <TableCell>Username</TableCell>
+                        <TableCell>Email</TableCell>
+                        <TableCell>Edit</TableCell>
+                        <TableCell>Delete</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {
+                        data.map( ({id, username, email}, idx) => (
+                            <TableRow key={idx}>
+                                <TableCell>{id}</TableCell>
+                                <TableCell>{username}</TableCell>
+                                <TableCell>{email}</TableCell>
+                                <TableCell>
+                                    <IconButton onClick={(e) => handleEdit(e)} value={id}>
+                                        <EditIcon/>
+                                    </IconButton>
+                                </TableCell>
+                                <TableCell>
+                                    <IconButton onClick={(e) => handleDelete(e)} value={id}>
+                                        <DeleteIcon sx={{color: 'red'}} />
+                                    </IconButton>
+                                </TableCell>
+                            </TableRow>
+                        ))
+                    }
+                </TableBody>
+            </Table>
+        </TableContainer>
+    )
+}
