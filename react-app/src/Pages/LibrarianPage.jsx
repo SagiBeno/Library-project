c4db5428fe809f20b1fbdc1fe78f9fadfb74bc37
SagiBeno <benosagi321@gmail.com>
2025-12-17 19:09:44 +0100

    Add LibrarianPage

---
+++ react-app/src/Pages/LibrarianPage.jsx
@@ -0,0 +1,9 @@
+import { Container } from "@mui/material"
+
+export default function LibrarianPage() {
+    return (
+        <Container className="container">
+            TODO - Modification of loan deadlines
+        </Container>
+    )
+}
