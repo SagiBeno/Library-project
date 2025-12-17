1a414f21eb8680845bbbc514dd64e37b6706091f
SagiBeno <benosagi321@gmail.com>
2025-12-17 13:06:13 +0100

    Add LendingCards component and integrate into BookLendingPage for displaying lended books

--- react-app/src/Components/Cards.jsx
+++ react-app/src/Components/Cards.jsx
@@ -1,7 +1,8 @@
-import { Card, CardMedia, CardHeader, CardContent, CardActions, Typography, Button, Box, Stack, Chip } from "@mui/material";
+import { Card, CardMedia, CardHeader, CardContent, CardActions, Typography, Button, Box, Stack, Chip, IconButton } from "@mui/material";
 import { useEffect, useState } from "react";
+import DeleteIcon from '@mui/icons-material/Delete';
 
-export default function Cards({ book, handleLendBook, lendedBooks }) {
+export function SearchCards({ book, handleLendBook, lendedBooks }) {
     const [imageUrl, setImageUrl] = useState('');
 
     useEffect(() => {
@@ -12,8 +13,6 @@ export default function Cards({ book, handleLendBook, lendedBooks }) {
             setImageUrl('https://www.globaluniversityalliance.org/wp-content/uploads/2017/10/No-Cover-Image-01.png');
         }
     }, []);
-
-
 
     return (
         <Card
@@ -47,9 +46,14 @@ export default function Cards({ book, handleLendBook, lendedBooks }) {
             <CardContent sx={{ flexGrow: 1 }}>
                 {
                     book?.author_name &&
-                    <Typography variant="subtitle1">
-                        {book?.author_name[0]}
-                    </Typography>
+                    <>
+                        <Typography variant="subtitle1">
+                            Author
+                        </Typography>
+                        <Typography variant="body2">
+                            {book?.author_name[0]}
+                        </Typography>
+                    </>
 
                 }
 
@@ -130,5 +134,81 @@ export default function Cards({ book, handleLendBook, lendedBooks }) {
             </CardActions>
         </Card >
 
+    )
+}
+
+export function LendingCards({ handleRemove, lendedBook }) {
+    const [imageUrl, setImageUrl] = useState('');
+
+    useEffect(() => {
+        if (lendedBook.cover_edition_key) {
+            const url = `https://covers.openlibrary.org/b/olid/${lendedBook.cover_edition_key}-M.jpg`;
+            setImageUrl(url);
+        } else {
+            setImageUrl('https://www.globaluniversityalliance.org/wp-content/uploads/2017/10/No-Cover-Image-01.png');
+        }
+    }, []);
+
+    return (
+        <Card
+            sx={{
+                width: '300px',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                textAlign: 'center',
+                margin: '10px',
+                boxShadow: '0px 0px 10px 0px gray',
+                padding: '5px',
+            }}
+            className="cards"
+        >
+            <CardHeader title={lendedBook.title} />
+
+            <CardMedia
+                component="img"
+                image={imageUrl}
+                alt={lendedBook.title}
+                sx={{
+                    width: '60%',
+                    maxHeight: '400px',
+                    margin: '0 auto',
+                }}
+                title={lendedBook.title}
+                loading='lazy'
+            />
+
+            <CardContent sx={{ flexGrow: 1 }}>
+                {
+                    lendedBook?.author_name &&
+                    <>
+                        <Typography variant="subtitle1">
+                            Author
+                        </Typography>
+                        <Typography variant="body2">
+                            {lendedBook?.author_name[0]}
+                        </Typography>
+                    </>
+                    
+
+                }
+            </CardContent>
+
+            <CardActions
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: 1,
+                    mt: 'auto',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}
+            >
+                <IconButton onClick={() => handleRemove(lendedBook)}>
+                    <DeleteIcon sx={{color: 'red'}}/>
+                </IconButton>
+
+            </CardActions>
+        </Card >
     )
 }
