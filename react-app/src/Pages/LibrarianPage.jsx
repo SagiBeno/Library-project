ccfa0fb6e21f053d545c9c51131a44b223c5fc20
SagiBeno <benosagi321@gmail.com>
2025-12-17 19:11:01 +0100

    Add TODO

--- react-app/src/Pages/LibrarianPage.jsx
+++ react-app/src/Pages/LibrarianPage.jsx
@@ -3,7 +3,7 @@ import { Container } from "@mui/material"
 export default function LibrarianPage() {
     return (
         <Container className="container">
-            TODO - Modification of loan deadlines
+            TODO - Modification of loan deadlines, Viewing and deleting loans
         </Container>
     )
 }
