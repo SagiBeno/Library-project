04eaabc1a6fb3267d4083792cfbb1ca6c04f5f27
SagiBeno <benosagi321@gmail.com>
2025-12-17 11:52:31 +0100

    Refactor Navbar component to conditionally render menu items based on login status

--- react-app/src/Components/Navbar.jsx
+++ react-app/src/Components/Navbar.jsx
@@ -66,52 +66,105 @@ export default function Navbar({ setLoggedIn, isAdmin, loggedIn, lendedBooks }) 
                         </Typography>
 
                         <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                            <IconButton
-                                size="large"
-                                aria-label="account of current user"
-                                aria-controls="menu-appbar"
-                                aria-haspopup="true"
-                                onClick={handleOpenNavMenu}
-                                color="inherit"
-                            >
-                                <MenuIcon />
-                            </IconButton>
+                            {
+                                loggedIn &&
+                                <>
+                                    <IconButton
+                                        size="large"
+                                        aria-label="account of current user"
+                                        aria-controls="menu-appbar"
+                                        aria-haspopup="true"
+                                        onClick={handleOpenNavMenu}
+                                        color="inherit"
+                                    >
+                                        <MenuIcon />
+                                    </IconButton>
 
-                            <Menu
-                                id="menu-appbar"
-                                anchorEl={anchorElNav}
-                                anchorOrigin={{
-                                    vertical: 'bottom',
-                                    horizontal: 'left',
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'left',
-                                }}
-                                open={Boolean(anchorElNav)}
-                                onClose={handleCloseNavMenu}
-                                sx={{ display: { xs: 'block', md: 'none' } }}
-                            >   
-                                
-                                <MenuItem onClick={() => {
-                                    handleCloseNavMenu();
-                                    navigate('/search');
-                                }}>
-                                    <Typography sx={{ textAlign: 'center', color: 'black' }}>Search</Typography>
-                                </MenuItem>
+                                    <Menu
+                                        id="menu-appbar"
+                                        anchorEl={anchorElNav}
+                                        anchorOrigin={{
+                                            vertical: 'bottom',
+                                            horizontal: 'left',
+                                        }}
+                                        keepMounted
+                                        transformOrigin={{
+                                            vertical: 'top',
+                                            horizontal: 'left',
+                                        }}
+                                        open={Boolean(anchorElNav)}
+                                        onClose={handleCloseNavMenu}
+                                        sx={{ display: { xs: 'block', md: 'none' } }}
+                                    >
 
-                                {
-                                    loggedIn &&
-                                    <MenuItem onClick={() => {
-                                        handleCloseNavMenu();
-                                        navigate('/my-books');
-                                    }}>
-                                        <Typography sx={{ textAlign: 'center', color: 'black' }}>My Books</Typography>
-                                    </MenuItem>
-                                }
+                                        <MenuItem onClick={() => {
+                                            handleCloseNavMenu();
+                                            navigate('/search');
+                                        }}>
+                                            <Typography sx={{ textAlign: 'center', color: 'black' }}>Search</Typography>
+                                        </MenuItem>
 
-                            </Menu>
+                                        {
+                                            loggedIn &&
+                                            <MenuItem onClick={() => {
+                                                handleCloseNavMenu();
+                                                navigate('/my-books');
+                                            }}>
+                                                <Typography sx={{ textAlign: 'center', color: 'black' }}>My Books</Typography>
+                                            </MenuItem>
+                                        }
+
+                                    </Menu>
+
+                                    <IconButton
+                                        size="large"
+                                        aria-label="account of current user"
+                                        aria-controls="menu-appbar"
+                                        aria-haspopup="true"
+                                        onClick={handleOpenNavMenu}
+                                        color="inherit"
+                                    >
+                                        <MenuIcon />
+                                    </IconButton>
+
+                                    <Menu
+                                        id="menu-appbar"
+                                        anchorEl={anchorElNav}
+                                        anchorOrigin={{
+                                            vertical: 'bottom',
+                                            horizontal: 'left',
+                                        }}
+                                        keepMounted
+                                        transformOrigin={{
+                                            vertical: 'top',
+                                            horizontal: 'left',
+                                        }}
+                                        open={Boolean(anchorElNav)}
+                                        onClose={handleCloseNavMenu}
+                                        sx={{ display: { xs: 'block', md: 'none' } }}
+                                    >
+
+                                        <MenuItem onClick={() => {
+                                            handleCloseNavMenu();
+                                            navigate('/search');
+                                        }}>
+                                            <Typography sx={{ textAlign: 'center', color: 'black' }}>Search</Typography>
+                                        </MenuItem>
+
+                                        {
+                                            loggedIn &&
+                                            <MenuItem onClick={() => {
+                                                handleCloseNavMenu();
+                                                navigate('/my-books');
+                                            }}>
+                                                <Typography sx={{ textAlign: 'center', color: 'black' }}>My Books</Typography>
+                                            </MenuItem>
+                                        }
+
+                                    </Menu>
+                                </>
+                            }
+
                         </Box>
 
                         <AutoStoriesIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
@@ -137,23 +190,27 @@ export default function Navbar({ setLoggedIn, isAdmin, loggedIn, lendedBooks }) 
                         <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
                             {
                                 loggedIn &&
-                                <Button
-                                    onClick={() => {
-                                        handleCloseNavMenu();
-                                        navigate('/my-books');
-                                    }}
-                                    sx={{ my: 2, color: 'black', display: 'block' }}
-                                >
-                                    My Books
-                                </Button>
+                                <>
+                                    <Button
+                                        onClick={() => {
+                                            handleCloseNavMenu();
+                                            navigate('/my-books');
+                                        }}
+                                        sx={{ my: 2, color: 'black', display: 'block' }}
+                                    >
+                                        My Books
+                                    </Button>
+
+                                    <Button
+                                        onClick={() => navigate('/search')}
+                                        sx={{ my: 2, color: 'black', display: 'block' }}
+                                    >
+                                        Search
+                                    </Button>
+                                </>
                             }
 
-                            <Button
-                                onClick={() => navigate('/search')}
-                                sx={{ my: 2, color: 'black', display: 'block' }}
-                            >
-                                Search
-                            </Button>
+
                         </Box>
 
                         {
@@ -162,7 +219,7 @@ export default function Navbar({ setLoggedIn, isAdmin, loggedIn, lendedBooks }) 
                                 {
                                     lendedBooks.length > 0 &&
                                     <Tooltip title="Requested book(s)...">
-                                        <IconButton sx={{ p: 0, color: 'black', marginRight: '20px' }} onClick={() => { navigate('/lending',  { state: { books: lendedBooks } }); }}>
+                                        <IconButton sx={{ p: 0, color: 'black', marginRight: '20px' }} onClick={() => { navigate('/lending', { state: { books: lendedBooks } }); }}>
                                             <StyledBadge badgeContent={lendedBooks.length} color="secondary">
                                                 <ShoppingCartIcon />
                                             </StyledBadge>
