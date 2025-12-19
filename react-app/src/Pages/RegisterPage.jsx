c75b7be0a4911b6bbb794b35cef2f31982c02f6a
SagiBeno <benosagi321@gmail.com>
2025-12-19 18:46:56 +0100

    Update snackbar handling and navigation after registration success

--- react-app/src/Pages/RegisterPage.jsx
+++ react-app/src/Pages/RegisterPage.jsx
@@ -42,15 +42,17 @@ export default function RegisterPage({ setIsLoading, snackbar, setSnackbar }) {
                 }
                 else {
                     setSnackbar({
+                        ...snackbar,
                         open: true,
                         message: 'Registration successful! You can now log in.',
                         severity: 'success',
                     });
-                    navigate('/login');
+                    navigate('/');
                 }
             })
             .catch((error) => {
                 setSnackbar({
+                    ...snackbar,
                     open: true,
                     message: 'An error occurred! Please try again!',
                     severity: 'error',
