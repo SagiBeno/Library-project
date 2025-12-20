03ee9205433d02ba8240beed7ec9abab1645f1be
SagiBeno <benosagi321@gmail.com>
2025-12-20 22:11:38 +0100

    Modify JSDoc

--- react-app/netlify/functions/get_all_borrow_for_user.js
+++ react-app/netlify/functions/get_all_borrow_for_user.js
@@ -1,15 +1,19 @@
 /**
- * Delete a borrow record by ID.
+ * Get all borrowed books for a user by username.
  *
- * Removes a borrow entry from the `library_project_borrows` table.
- * Intended for administrative/librarian use (or internal cleanup).
+ * Looks up a user in the `library_project_users` table by `username`,
+ * retrieves all related borrow records from `library_project_borrows`,
+ * and returns the `external_key` values of the borrowed books
+ * from the `library_project_books` table.
  *
- * @route DELETE /.netlify/functions/delete_borrow
+ * @route POST /.netlify/functions/get_user_borrows
  *
- * @param {Request} request - Netlify Function request
- * @bodyParam {number} id - Borrow record ID to delete
+ * @param {Request} request - Netlify Function request object
+ * @bodyParam {string} username - Username of the user whose borrows are requested
  *
- * @response 200 application/json Borrow record deleted successfully
+ * @response 200 application/json Successfully retrieved borrowed book keys
+ * @response 400 application/json Username is required
+ * @response 404 application/json User not found
  * @response 405 application/json Method not allowed
  * @response 500 application/json Database error
  */
