ed496db334ba229a221d3c83a13e62fb65aa9997
SagiBeno <benosagi321@gmail.com>
2025-12-20 22:14:39 +0100

    Adding JSDoc to get_borrow.js

--- react-app/netlify/functions/get_borrow.js
+++ react-app/netlify/functions/get_borrow.js
@@ -1,3 +1,20 @@
+/**
+ * Get borrow records by book ID.
+ *
+ * Retrieves all borrow entries from the `library_project_borrows` table
+ * that are associated with the specified `book_id`.
+ *
+ * @route POST /.netlify/functions/get_borrow
+ *
+ * @param {Request} request - Netlify Function request object
+ * @bodyParam {number} book_id - ID of the book whose borrow records are requested
+ *
+ * @response 200 application/json Successfully retrieved borrow records
+ * @response 400 application/json Book ID is required
+ * @response 405 application/json Method not allowed
+ * @response 500 application/json Database error
+ */
+
 //POST get_borrow method
 
 import { createClient } from "@supabase/supabase-js";
